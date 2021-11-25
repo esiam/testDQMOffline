85238872ff960ce787784a26e2ea4860d3704f94
esiam <78793297+esiam@users.noreply.github.com>
2021-11-26 01:39:49 +0200

    Update HTMonitoring_Client_cff.py
    "add eff"

--- Trigger/python/HTMonitoring_Client_cff.py
+++ Trigger/python/HTMonitoring_Client_cff.py
@@ -6,10 +6,18 @@ htEfficiency = DQMEDHarvester("DQMGenericClient",
     verbose        = cms.untracked.uint32(0), # Set to 2 for all messages
     resolution     = cms.vstring(),
     efficiency     = cms.vstring(
-        "effic_ht          'HT turnON;            PF HT [GeV]; efficiency'     ht_numerator          ht_denominator",
-        "effic_ht_variable 'HT turnON;            PF HT [GeV]; efficiency'     ht_variable_numerator ht_variable_denominator",
-        "effic_deltaphimetj1          'DELTAPHI turnON;            DELTA PHI (PFMET, PFJET1); efficiency'     deltaphimetj1_numerator          deltaphimetj1_denominator",
-        "effic_deltaphij1j2          'DELTAPHI turnON;            DELTA PHI (PFJET1, PFJET2); efficiency'     deltaphij1j2_numerator          deltaphij1j2_denominator"
+        "effic_ht                     'HT turnON;                  PF HT [GeV]; efficiency'                      ht_numerator                   ht_denominator",
+        "effic_ht_variable            'HT turnON;                  PF HT [GeV]; efficiency'                      ht_variable_numerator          ht_variable_denominator",
+        "effic_deltaphi_met_jet1      'DELTAPHI turnON;            DELTA PHI (PFMET, PFJET1); efficiency'        deltaphi_met_jet1_numerator    deltaphi_met_jet1_denominator",
+        "effic_deltaphi_jet1_jet2     'DELTAPHI turnON;            DELTA PHI (PFJET1, PFJET2); efficiency'       deltaphi_jet1_jet2_numerator   deltaphi_jet1_jet2_denominator",
+        "effic_jetPt1                 'JET_PT1 turnON;             LEADING JET PT [GeV]; efficiency'             jetPt1_numerator               jetPt1_denominator",
+        "effic_jetPt2                 'JET_PT2 turnON;             SUBLEADING JET PT [GeV]; efficiency'          jetPt2_numerator               jetPt2_denominator",
+        "effic_jeteta1                'JET_ETA1 turnON;            LEADING JET #eta; efficiency'                 jeteta1_numerator              jeteta1_denominator",
+        "effic_jeteta2                'JET_ETA2 turnON;            SUBLEADING JET #eta; efficiency'              jeteta2_numerator              jeteta2_denominator",
+        "effic_jetPhi1                'JET_PHI1 turnON;            LEADING JET #phi; efficiency'                 jetPhi1_numerator              jetPhi1_denominator",
+        "effic_jetPhi2                'JET_PHI2 turnON;            SUBLEADING JET #phi; efficiency'              jetPhi2_numerator              jetPhi2_denominator",
+        "effic_nJets                  'nJets;                      nJets; efficiency'                            nJets_numerator                nJets_denominator",
+        "effic_nJets_HT               'nJets (Pt>30 && eta<2.5);   nJets_; efficiency'                           nJetsHT_numerator              nJetsHT_denominator"
     ),
     efficiencyProfile = cms.untracked.vstring(
         "effic_ht_vs_LS 'HT efficiency vs LS; LS; PF HT efficiency' htVsLS_numerator htVsLS_denominator"
